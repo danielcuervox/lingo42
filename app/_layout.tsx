9cb76661452598e6fca6dea61620ab7d987b2837
danielcuervox <danielcuervox@hotmail.com>
2026-09-15 17:40:56 +0200

    user's request sent

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,14 @@
 import "../global.css";
 import { Stack } from "expo-router";
 import "react-native-reanimated";
+import { LanguageProvider } from "../context/LanguageContext";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="index" />
-    </Stack>
+    <LanguageProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+      </Stack>
+    </LanguageProvider>
   );
 }
